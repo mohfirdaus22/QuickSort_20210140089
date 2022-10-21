21fd4151b9831d5735bc08b983c11e5d35122afb
mohfirdaus22 <thepashter6@gmail.com>
2022-10-21 17:39:10 +0700

    add move pivot, and sort the list on

--- QuickSort_20210140089/Program.cs
+++ QuickSort_20210140089/Program.cs
@@ -90,7 +90,22 @@ class program
                     mov_count++;
                 }
             }
+
+            // j  now contains the index of the last element in the sorted list
+
+            if (low < j)
+            {
+                //move to pivot to its correct posisition in the list
+                swap (low, j);
+                mov_count++;
+            }
+            //sort the list on the left of pivot using quick sort
+            q_sort(low, j-1);
+
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1, high);
         }
+
         
     }
 }
