0eed14d988ff16edbe55e1e6ceb3d1507d7298ac
mohfirdaus22 <thepashter6@gmail.com>
2022-10-21 16:56:05 +0700

    add class program

--- QuickSort_20210140089/Program.cs
+++ QuickSort_20210140089/Program.cs
@@ -5,5 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace QuickSort
-{ }
+{
+class program
+    {
+        // array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0;  //number of comparation
+        private int mov_count = 0;  // number of data movement
+
+        //Numbers of elements in array
+        private int n;
+    }
+}
 
